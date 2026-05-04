4e46b20c9b2f9c9c3ba5b5a38e75b3ed383bc844
Tiksha <tiksha0505@gmail.com>
2026-05-04 22:18:08 +0530

    Update code

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Transaction = require('../models/transaction');
+const kmeans = require('ml-kmeans');
 
 //api endpoint for saving an expense
 router.post('/', async (req, res) => {
@@ -18,6 +19,38 @@ router.post('/', async (req, res) => {
 router.get('/', async(req, res) => {
     const data = await Transaction.find();
     res.json(data);
+});
+
+// k means clustering logic
+router.get('/analyze', async(req, res) => {
+    try {
+        const txns = await transaction.find();
+
+        if (txns.length < 3) {
+            return res.status(400).json({ message: "Add atleast 3 expenses to analyze!"});
+        }
+    
+
+        // preparing data: kmeans need a 2d array
+        const dataML = txns.map(t => [t.amount]);
+
+        // run k means with 3 clusters
+        const result = kmeans(dataML, 3);
+
+        //update each transaction with its new cluster ID
+        const updatePromises = transaction.map((t, index) => {
+            return transaction.findByIdAndUpdate(t._id, {
+                clusterID: result.clsuters[index]
+            });
+        });
+
+        await Promise.all(updatePromises);
+
+        res.json({message: "Analysis complete!", clusters: result.clusters});
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+
 });
 
 module.exports = router;
